ef28f9c90c62617b87f00eb470f3fdc76f72b480
HumamShwaikh <hshwa039@uottawa.ca>
2018-11-27 16:56:45 +0000

    Card header

---
+++ card.h
@@ -0,0 +1,32 @@
+/******************************
+Title:   Final Project
+Course:  CSI 2372 A - Fall 2018
+Date:    December 3, 2018
+Contributors:
+Name:    Matthew Smith
+SID:     7650838
+Email:   msmit245@uottawa.ca
+Name:    Humam Shwaikh
+SID:     7867034
+Email:   hshwa039@uottawa.ca
+File Number C of X
+******************************/
+
+#ifndef CARD
+#define CARD
+
+#include <string>
+
+using std::string;-
+
+class Card {
+    
+	enum FaceAnimal {Crab, Penguin, Octopus, Turtle, Walrus};
+	enum FaceBackground {Red, Green, Purple, Blue, Yellow};
+	
+	Card(Animal a, Colour b);
+
+};
+
+
+#endif
