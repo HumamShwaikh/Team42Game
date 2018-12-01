6963906aa247edc115144d2b14bc54e7e5618695
humam <hshwa039@uottawa.ca>
2018-11-30 20:16:15 -0500

    created card deck

---
+++ carddeck.cpp
@@ -0,0 +1,23 @@
+/******************************
+
+Title:   Final Project
+Course:  CSI 2372 A - Fall 2018
+Date:    December 3, 2018
+
+Contributors:
+
+Name:    Matthew Smith
+SID:     7650838
+Email:   msmit245@uottawa.ca
+
+Name:    Humam Shwaikh
+SID:     7867034
+Email:   hshwa039@uottawa.ca
+
+File Number 5 of X
+
+******************************/
+
+#include "card.h"
+
+
