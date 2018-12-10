91425898b41100443265f18f0e129b4c5b249545
HumamShwaikh <hshwa039@uottawa.ca>
2018-12-10 02:41:45 +0000

    update with matt's

--- board.h
+++ board.h
@@ -1,21 +1,15 @@
 /******************************
-
 Title:   Final Project
 Course:  CSI 2372 A - Fall 2018
 Date:    December 3, 2018
-
 Contributors:
-
 Name:    Matthew Smith
 SID:     7650838
 Email:   msmit245@uottawa.ca
-
 Name:    Humam Shwaikh
 SID:     7867034
 Email:   hshwa039@uottawa.ca
-
 File Number 8 of X
-
 ******************************/
 
 #ifndef BOARD
